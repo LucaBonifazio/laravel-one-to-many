e1bdc684f7285edf4914ba003f9d747cc5e12400
LucaBonifazio <luca.bonifazio13@gmail.com>
2023-01-30 17:28:43 +0100

    add <h3> on post/ show.blade

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <div class="container">
         <h1>{{ $post->title }}</h1>
+        <h3>Category: {{ $post->category->name }}</h3>
         <img src="{{ $post->image }}" alt="{{ $post->title }}">
         <img src="{{ asset('storage/' . $post->uploaded_img) }}" alt="{{ $post->title }}">
         <p>{{ $post->content }}</p>
